c5fe6045b70aabdc6b0d458c311ffaf3cb13acf3
julian <juliansolo7189@gmail.com>
2024-10-02 10:57:37 -0500

    variable nombre python

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Se asigna el valor "julian" a la variable nombre
+nombre = "julian"
+
+# Se asigna el valor "velez g" a la variable apellido
+apellido = "velez g"
+
+# Se imprime el valor de las variables 'nombre' y 'apellido', separados por un espacio por defecto
+print(nombre, apellido)
